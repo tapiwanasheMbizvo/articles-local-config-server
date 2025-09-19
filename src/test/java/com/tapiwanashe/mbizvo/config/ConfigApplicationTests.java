f85c19702a9278418560cc224769a4259fe6ce8d
tapiwa <tapiwambizvo@gmial.com>
2025-09-19 05:33:33 +0200

    dockerise app

---
+++ src/test/java/com/tapiwanashe/mbizvo/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapiwanashe.mbizvo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
